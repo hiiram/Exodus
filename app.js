5e3ae367c7bbccb8cd54ed4a81d41bbb8a4620c5
Carlos <cllanochicaiza@gmail.com>
2015-09-08 00:57:49 +0200

    Primera Subida

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-console.log("Hola ");
+console.log("Hola mundo ");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
